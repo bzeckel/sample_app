7a69822371eafae8880c0d2345f07e95897ee6d7
Ben Zeckel <Ben.Zeckel@gmail.com>
2014-03-03 09:07:31 -0500

    test links on home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,21 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign Up'))
+      click_link "sample app"
+      expect(page).to have_title(full_title(''))
+    end
   end
 
   describe "Help page" do
@@ -44,4 +59,5 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
+
 end
